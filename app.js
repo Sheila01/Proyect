1eee9f9b9bb9203320b15b4410e21894d539d72c
Sheila Colorado <sheilacolorado1997@gmail.com>
2023-03-14 11:14:21 +0100

    I changed the structure and touched something that doesn't work.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-import express from 'express'
-import cors from 'cors';
-
-const app = express();
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+app.use(cors())
+const app = express()
 
 app.use(cors())
 
-const port = 3001
+const port = process.env.PORT 
 
 app.listen(port, () => {
   console.log(`Ey bro your port  is: http://localhost ${port}`)
