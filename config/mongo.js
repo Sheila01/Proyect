e69a37c26cfd5ba1b0b8a35f539329491dfc318c
Sheila Colorado <sheilacolorado1997@gmail.com>
2023-03-17 23:29:48 +0100

    I don't understand and the other changes don't even come out

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const dbConnect = () => {
-    const DB_URI = process.env.DB_URI
+const  dbConnect = async() => {
+    const DB_URI = await process.env.DB_URI
     mongoose.connect(DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
