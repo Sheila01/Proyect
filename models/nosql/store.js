b1c9b67bacb9f03b462cde9988262b93f3183f75
Sheila Colorado <sheilacolorado1997@gmail.com>
2023-03-18 02:21:45 +0100

    Creating the store file and adding the schema containing a url and a filename

---
+++ models/nosql/store.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const StoreSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String
+    },
+    filename: {
+      type: Number
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+
+//module.esports = mongoose.model('Store', StoreSchema)
+export default {
+  Store,
+  StoreSchema
+}
